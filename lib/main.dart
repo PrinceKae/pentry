ad4371632a5d511e23d79c0b9b9afce1557aedec
princekae <pkebayzim@gmail.com>
2023-04-23 09:52:36 +0100

    Creation of the input field and intl date was added to customise the date displayed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '/transaction.dart';
 
 void main() => runApp(MyApp());
@@ -36,6 +37,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -45,10 +47,54 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
+      //Textfield is an input field, inorder to create an input field, you would have to do it under the container of the AppBar.
+          Card(child: Column(children: <Widget>[
+            TextField(),
+            TextField(),
+          ],),),
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                //row to hold my widget decoration and values
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black54,
+                          width: 2,
+                        )),
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      //string interpolation with the use of $ and \ to add a total sign to your amount
+                      '\$${tx.amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.deepPurpleAccent),
+                    ),
+                  ),
+                  //this column contains the values for title and the above is for amount
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        //an intl package was installed and the DateFormat string was use and tx.date was called
+                        DateFormat.yMMMMd().format(tx.date),
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ]),
               );
             }).toList(),
           ),
